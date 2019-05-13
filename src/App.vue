a5bda09c17672c08e33429bfa3209a330d9bbad9
Andy Gray <andy@whatsthescore.co.uk>
2019-05-13 11:58:28 +0100

    pre-web3 version

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
 
                     <div class="row">
                         <div class="col">
-                            <b-form-group id="nation" label="Nation:" label-for="nation">
+                            <b-form-group id="nation" label="Nationality:" label-for="nation">
                                 <b-form-select id="nation" :options="niftyData.nationalityOptions" required v-model="nationality"/>
                             </b-form-group>
                         </div>
@@ -38,7 +38,7 @@
                     <div class="row">
                         <div class="col-2">
                             <b-form-group id="ethnicity" label="Ethnicity:" label-for="ethnicity">
-                                <b-form-select id="ethnicity" :options="niftyData.ethnicitiesOptions" required v-model="ethnicity" v-on:change="ethnicitiesChange"/>
+                                <b-form-select id="ethnicity" :options="niftyData.ethnicitiesOptions" required v-model="ethnicity"/>
                             </b-form-group>
                         </div>
                         <div class="col">
@@ -49,115 +49,23 @@
                         <div class="col">
                             <b-form-group id="colours" label="Colours:" label-for="colours">
                                 <b-form-select id="colours" :options="niftyData.coloursOptions" required v-model="colour"/>
-                                <!--<swatches v-model="niftyData.colours[colour].primary"></swatches>-->
-                                <!--<swatches v-model="niftyData.colours[colour].secondary"></swatches>-->
-                                <!--<swatches v-model="niftyData.colours[colour].tertiary"></swatches>-->
                             </b-form-group>
                         </div>
                     </div>
 
-
-                    <!--<div style="display: none">-->
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Skin:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.skin[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.skin[1]" type="number"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.skin[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Shadow:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.shadow[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.shadow[1]" type="number"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.shadow[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Cheek:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.cheek[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.cheek[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.cheek[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Eye:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.eye[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.eye[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.eye[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Hair Top:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.hair_top[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.hair_top[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.hair_top[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Hair Bottom:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.hair_bottom[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.hair_bottom[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.hair_bottom[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Beard:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.beard[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.beard[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.beard[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Tache:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.tache[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.tache[1]" type="text"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.tache[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-
-                        <!--<div class="row">-->
-                            <!--<div class="col">-->
-                                <!--Stubble:-->
-                                <!--<b-form-input v-model="ethnicitiesClone.stubble[0]" type="text"/>-->
-                            <!--</div>-->
-                            <!--<div class="col">-->
-                                <!--<b-form-input v-model="ethnicitiesClone.stubble[1]" type="number"/>-->
-                                <!--<swatches v-model="ethnicitiesClone.stubble[0]"></swatches>-->
-                            <!--</div>-->
-                        <!--</div>-->
-                    <!--</div>-->
                 </b-form>
+
+                <hr/>
+
+                <code>
+                    firstName: {{firstName}}<br/>
+                    lastName: {{lastName}}<br/>
+                    nationality: {{nationality}}<br/>
+                    position: {{position}}<br/>
+                    ethnicity: {{ethnicity}}<br/>
+                    kit: {{kit}}<br/>
+                    colour: {{colour}}<br/>
+                </code>
             </div>
         </div>
     </div>
@@ -165,8 +73,6 @@
 
 <script>
     import axios from 'axios';
-    import Swatches from 'vue-swatches';
-    import 'vue-swatches/dist/vue-swatches.min.css';
 
     export default {
         name: 'app',
@@ -185,15 +91,8 @@
                 ethnicitiesClone: {},
             };
         },
-        components: {
-            Swatches
-        },
-        methods: {
-            ethnicitiesChange (c) {
-                this.ethnicitiesClone = this.niftyData.nations[this.nationality].ethnicities;
-                // console.log(this.ethnicitiesClone);
-            }
-        },
+        components: {},
+        methods: {},
         async created () {
             const res = await axios.get(`${this.rootApi}/network/1/image/data`);
             this.niftyData = res.data;
@@ -211,7 +110,7 @@
             Object.keys(this.niftyData.nations[this.nationality].ethnicities).forEach((k) => this.niftyData.ethnicitiesOptions.push({value: k, text: k}));
 
             this.niftyData.positionOptions = [];
-            Object.keys(this.niftyData.positions).forEach((k) => this.niftyData.positionOptions.push({value: this.niftyData.positions[k], text: k}));
+            Object.keys(this.niftyData.positions).forEach((k, i) => this.niftyData.positionOptions.push({value: this.niftyData.positions[k], text: `${i} - ${k}`}));
 
             this.niftyData.firstNameOptions = [];
             Object.keys(this.niftyData.nations[this.nationality].firstNames).forEach((k) => this.niftyData.firstNameOptions.push({value: k, text: `${k} - ${this.niftyData.nations[this.nationality].firstNames[k].latin}`}));
@@ -226,9 +125,5 @@
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         color: #2c3e50;
-    }
-
-    .vue-swatches {
-        display: inline-block;
     }
 </style>
